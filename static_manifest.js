ccb0b7156cc3fb797125ad167e2be41b2e98c376
jjhl-yycdg <2512008347@qq.com>
2025-11-05 03:11:20 +0800

    Add files via upload

---
+++ static_manifest.js
@@ -0,0 +1,27 @@
+// Inline static manifest to support opening the site via file://
+// Regenerate this file when you add/remove files in the `album/` folder.
+window.INLINE_STATIC_MANIFEST = [
+  "01982B849496623E48E3781D71167EC8.png",
+  "1a9407620db69ee1ccef2c5c168209c1.jpeg",
+  "214_1eaa9350b8bed211561c63d26ab7f993.png",
+  "215_d5c417207d19920102102f8abc3271be.png",
+  "3E1068FD0AEF22D7D13E9037323F566B.png",
+  "3e64db45c818542bda8605f79a5801d5_40a667705b19f4802238ed05eba97679.png",
+  "3EF947D0B1B7AC839EC4D549AFE95DAE.png",
+  "3_46b282a14a5a355fd74e587caf62899c.png",
+  "4_b4927fde6bb09fd9b75d6da9e5bd8ae0.png",
+  "7E88FED58FFAC7B724ED71A990CEDDF1.png",
+  "9cf55e87dc02f9279db2e5f0cbd13927.png",
+  "c32c1797ce8e2e24f5a01af10f112c6e.png",
+  "d81f301c168d84735e9e2bb6fdfa5415.png",
+  "edf9520db41e718670f0a7e45c6270c8_671f7e031eafd371fc02dfbeab22a3b7.png",
+  "Image_2323807118583047.png",
+  "Image_2323811992542740.png",
+  "img_0579_90d73fc46c2d85be27f2d846dec5f378.png",
+  "img_0580_c3a53d7fa120f56eef681cc3c8599709.png",
+  "img_4411_b437250b57fa14c0e02b24426078d94f.png",
+  "img_5938.png",
+  "img_9776_c7613e5123b9102a0bc010955acc5b4a.png",
+  "微信图片_20250903203609.jpg",
+  "插画34.png"
+];
